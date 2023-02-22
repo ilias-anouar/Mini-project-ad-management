461068682a7fa3f428275cd8b29002a5c5e7b10b
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-02-22 09:56:47 +0100

    ADD

--- index.php
+++ index.php
@@ -13,35 +13,50 @@ $result = $conn->query($sql);
 
 
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-  echo "<pre>";
-  var_dump($row);
-  echo "</pre>";
+    echo "<pre>";
+    var_dump($row);
+    echo "</pre>";
 
 
-  ?>
+    ?>
     <div>
 
-<div class="container">
-<div class="col-auto col-sm-12 col-md-12 col-lg-4 col-xl-4" style="padding-top: 15px;padding-bottom: 15px;padding-right: 15px;padding-left: 15px;">
-    <div class="bg-light border rounded shadow card" data-bss-hover-animate="pulse">
-        <img class="card-img-top mb-3" src="#" width="400" height="278">
-        <div class="card-body">
-            <div id="icon-span"> <p id="type-an"><?php echo $row['type'];?></p></div>
-            <h3 class="card-title" style="font-family: Antic, sans-serif;color: rgb(81,87,94);"><?php echo $row['title'];?></h3>
-            <h5 class="card-sub-title" style="font-family: Antic, sans-serif;color: #38ae53;margin: 13px;margin-top: 10px;margin-right: 20px;margin-bottom: 18px;margin-left: 43px;font-size: 23px; width: 100%; ">
-                <strong><?php echo $row['category'];?>  | <?php echo $row['price'];?> $ </strong></h5>
-                <p class="card-text" style="font-family: Antic, sans-serif;color: rgb(81,87,94);"><?php echo $row['City'];?></p>
-                <button class="btn btn-danger" id="Edit" style="border: none;width: 100px;height: 38px;background: #9ecad5;" type="button" data-target="#Edit<?php echo $row['ad_id'] ?>">Edite</button>
-                <button class="btn btn-danger" id ="Details" style="border: none;width: 100px;height: 38px;margin-left: 14px;color: A63F04;background: #A63F04;"type="button" data-target="#Details<?php echo $row['ad_id']?>">Details</button>
-                <button class="btn btn-outline-success" id="Delete" type="button" style="font-weight: normal;font-family: Antic, sans-serif;width: 100px;margin: 22px;" data-target="#Delete<?php echo $row['ad_id'] ?>">Delete</button>
+        <div class="container">
+            <div class="col-auto col-sm-12 col-md-12 col-lg-4 col-xl-4"
+                style="padding-top: 15px;padding-bottom: 15px;padding-right: 15px;padding-left: 15px;">
+                <div class="bg-light border rounded shadow card" data-bss-hover-animate="pulse">
+                    <img class="card-img-top mb-3" src="#" width="400" height="278">
+                    <div class="card-body">
+                        <div id="icon-span">
+                            <p id="type-an">
+                                <?php echo $row['type']; ?>
+                            </p>
+                        </div>
+                        <h3 class="card-title" style="font-family: Antic, sans-serif;color: rgb(81,87,94);">
+                            <?php echo $row['title']; ?>
+                        </h3>
+                        <h5 class="card-sub-title"
+                            style="font-family: Antic, sans-serif;color: #38ae53;margin: 13px;margin-top: 10px;margin-right: 20px;margin-bottom: 18px;margin-left: 43px;font-size: 23px; width: 100%; ">
+                            <strong>
+                                <?php echo $row['category']; ?> |
+                                <?php echo $row['price']; ?> $
+                            </strong>
+                        </h5>
+                        <p class="card-text" style="font-family: Antic, sans-serif;color: rgb(81,87,94);">
+                            <?php echo $row['City']; ?>
+                        </p>
+                        <button class="btn btn-danger" id="Details"
+                            style="border: none;width: 100px;height: 38px;margin-left: 14px;color: A63F04;background: #A63F04;"
+                            type="button" data-target="#Details<?php echo $row['ad_id'] ?>">Details</button>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
- 
-</div>
-<?php
-};
+    <?php
+}
+;
 
 $pdo = null;
 ?>
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let addedimages = document.getElementById("inputGroupFileAddon04");
 addedimages.addEventListener("click", function () {
-  let image = document.getElementById("Image").value;
+  let image = document.getElementById("Image");
   let div = document.getElementById("upload");
   let input = `<div class="input-group mb-3">
     <div class="input-group-text">
         <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
     </div>
-    <input type="text" class="form-control" aria-label="Text input with checkbox" value="${image}">
+    <input type="text" class="form-control" aria-label="Text input with checkbox" value="${image.value}">
 </div>`;
+  image.value = "";
   div.append(input);
 });
