e752c435de6115fca0d9eaebb4bfebac19ff1f89
zaani12 <116799506+zaani12@users.noreply.github.com>
2023-02-22 16:06:25 +0100

    ZAANI

--- delete.php
+++ delete.php
@@ -1,27 +1,18 @@
 <?php
-include "header.php";
-// include_once "./component.php";
 include "connect.php";
 
-// include "component.php";
-include "index.php";
-
-
-$id = $_GET['ad_id'];
-echo $id;
+include "clients.php";
+include 'index.php';
 
 // $sql = "SELECT * FROM annonce "
-$sql = "DELETE  * FROM `annonce` NATURAL JOIN `image_d_annonce` where 'ad_id'  = $id";
+$sql = "DELETE   FROM `annonce`  where 'ad_id'  = '$id'";
 
 //  LIMIT 3";
 $result = $conn->query($sql);
 
 
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    // echo "<pre>";
-    // var_dump($row);
-    // echo "</pre>";
+// while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 
-}
+// }
     ?>
     
